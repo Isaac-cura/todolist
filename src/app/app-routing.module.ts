97715da7c72e48350c40a59bd8f691d2fde70e21
José Isaac-cura <demon.mystery@gmail.com>
2021-04-15 10:33:19 -0400

    add module to global routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  redirectTo: 'authorization',
+  pathMatch: 'full'
+}, {
+  path: 'authorization',
+  loadChildren: () => import('./pages/authorization/authorization.module').then(m => m.AuthorizationModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
